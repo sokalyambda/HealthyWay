c3a4cf0bfff396e7c32c55a9f9ac322b0eb35137
Eugenity <eugenesokolenko92@gmail.com>
2016-06-24 12:36:08 +0300

    Initial Commit

---
+++ HealthyWay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HealthyWay
+//
+//  Created by Eugenity on 24.06.16.
+//  Copyright © 2016 Eugenity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HealthyWayTests/HealthyWayTests.m
@@ -0,0 +1,39 @@
+//
+//  HealthyWayTests.m
+//  HealthyWayTests
+//
+//  Created by Eugenity on 24.06.16.
+//  Copyright © 2016 Eugenity. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HealthyWayTests : XCTestCase
+
+@end
+
+@implementation HealthyWayTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
