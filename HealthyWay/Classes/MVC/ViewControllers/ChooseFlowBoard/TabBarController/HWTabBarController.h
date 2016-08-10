a0d3a32344882cd36bb579cd2ce99e7f3c803d87
AnastasiaMark <eugene@iMac.local>
2016-08-10 14:43:34 +0300

    Added ChooseFlowBoard. Created ChoosenFlowBoardStoryboard file and HWTabBarController class.

---
+++ HealthyWay/Classes/MVC/ViewControllers/ChooseFlowBoard/TabBarController/HWTabBarController.h
@@ -0,0 +1,12 @@
+//
+//  HWTabBarController.h
+//  HealthyWay
+//
+//  Created by Eugene Sokolenko on 10.08.16.
+//  Copyright © 2016 Eugenity. All rights reserved.
+//
+
+
+@interface HWTabBarController : UITabBarController
+
+@end
---
+++ HealthyWay/Classes/MVC/ViewControllers/ChooseFlowBoard/TabBarController/HWTabBarController.m
@@ -0,0 +1,22 @@
+//
+//  HWTabBarController.m
+//  HealthyWay
+//
+//  Created by Eugene Sokolenko on 10.08.16.
+//  Copyright © 2016 Eugenity. All rights reserved.
+//
+
+#import "HWTabBarController.h"
+
+@interface HWTabBarController ()
+
+@end
+
+@implementation HWTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+@end
