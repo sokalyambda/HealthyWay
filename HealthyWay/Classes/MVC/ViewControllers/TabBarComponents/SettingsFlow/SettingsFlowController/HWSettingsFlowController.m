3f7c4b31ac7672634b8b9cb238d59744bd66f162
AnastasiaMark <eugene@iMac.local>
2016-08-12 17:41:13 +0300

    Made Settings screen using the static table view controller as a child view controller of HWSettingsFlowController.

---
+++ HealthyWay/Classes/MVC/ViewControllers/TabBarComponents/SettingsFlow/SettingsFlowController/HWSettingsFlowController.m
@@ -0,0 +1,24 @@
+//
+//  HWSettingsFlowController.m
+//  HealthyWay
+//
+//  Created by Anastasia Mark on 12.08.16.
+//  Copyright © 2016 Eugenity. All rights reserved.
+//
+
+#import "HWSettingsFlowController.h"
+
+@interface HWSettingsFlowController ()
+
+@end
+
+@implementation HWSettingsFlowController
+
+#pragma mark - View Lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
---
+++ HealthyWay/Classes/MVC/ViewControllers/TabBarComponents/SettingsFlow/SettingsFlowTableViewController/HWSettingsFlowTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  HWSettingsFlowTableViewController.h
+//  HealthyWay
+//
+//  Created by Anastasia Mark on 12.08.16.
+//  Copyright © 2016 Eugenity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HWSettingsFlowTableViewController : UITableViewController
+
+@end
